3fd7981edc1fe1deb5a40dbd77c466a8ff802a58
alumno <alumno@3MASanz-A06-15>
2021-11-16 11:21:22 +0100

    Pregunta1

---
+++ Pregunta1.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Pregunta_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prog = @"C:\Program Files (x86)\Notepad++\notepad++.exe";
+            Process.Start(prog);
+        }
+    }
+}
